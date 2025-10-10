b05c40c77e80198c6814d1972223ef034c01c394
seoyarksa <yyulss@naver.com>
2025-10-10 14:27:54 +0900

    정기결제연동

--- api/payment/register-billing.js
+++ api/payment/register-billing.js
@@ -2,7 +2,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import { createClient } from '@supabase/supabase-js';
-const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_SERVICE_ROLE_KEY);
+const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_SERVICE_ROLE);
 
 export default async function handler(req, res) {
   if (req.method !== "POST") {
