dbf0a624fb4a41fa8b939a6743fc010edd2bf267
seoyarksa <yyulss@naver.com>
2025-11-01 07:52:47 +0900

    결제전환보정

--- public/sinsalUtils.js
+++ public/sinsalUtils.js
@@ -360,7 +360,7 @@ const midHeader = `
         <tr>
           <td>${label || ''}</td>
           <td>${bStem || '-'}${tenToShow ? ` <span class="ten-god">(${tenToShow})</span>` : ''}</td>
-          ${cells.map(u => `<td><span class="unseong-tag">${u}</span></td>`).join('')}
+          ${cells.map(u => `<td><span class="unseong-tag explainable" data-group="unseong" data-term="${u}">${u}</span></td>`).join('')}
         </tr>
       `;
     }).join('');
