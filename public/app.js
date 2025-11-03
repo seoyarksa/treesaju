141ab26ad31b224c394db5199919d9d445679868
seoyarksa <yyulss@naver.com>
2025-11-03 16:18:39 +0900

    결제전환보정

--- public/app.js
+++ public/app.js
@@ -5077,7 +5077,7 @@ const sajuGanjiArr = [
   saju.yearGan + saju.yearBranch
 ];
 document.getElementById('sinsal-box').innerHTML = renderSinsalTable({ sajuGanArr, samhapKey, sajuJijiArr });
-
+wireSajuTooltips(container);
 
 
 
