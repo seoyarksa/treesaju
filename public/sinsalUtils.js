909153ce448b483fb6a6ad9eaf40dc0aa30404c9
seoyarksa <yyulss@naver.com>
2025-11-05 07:58:31 +0900

    미니창 생서

--- public/sinsalUtils.js
+++ public/sinsalUtils.js
@@ -305,7 +305,7 @@ const header = `
   <tr>
     <th rowspan="2" style="min-width:72px; background:#e9f5ff;">기준</th>
     <th rowspan="2" style="min-width:44px; background:#e9f5ff;">값</th>
-    ${colLabels.map(lbl => `<th style="min-width:56px; background:#e9f5ff;">${lbl}</th>`).join('')}
+    ${colLabels.map(lbl => `<th style="min-width:56px; background:#e9f5ff;"><span class="explainable" data-group="ganji" data-term="${lbl}">${lbl}</span></th>`).join('')}
   </tr>
   <tr>
     ${branches.map((br, i) =>
