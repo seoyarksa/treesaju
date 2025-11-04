f384c6b2edbec5583c101430a114ea9e87929327
seoyarksa <yyulss@naver.com>
2025-11-04 16:13:58 +0900

    미니창 생서

--- public/app.js
+++ public/app.js
@@ -4843,7 +4843,11 @@ renderSajuMiniFromCurrentOutput({
         <td style="border:1px solid #ccc; padding:4px;">
           <div id="dangryeongshik-container" style="margin-top: 0.5rem;"></div>
         </td>
-        <td style="border:1px solid #ccc; padding:4px;"><div id="gyeok-flow"></div></td>
+        <td style="border:1px solid #ccc; padding:4px;"><div style="text-align:left; margin:8px 0; color:#0077cc;">
+  05] <span class="explainable" data-group="terms" data-term="">격국</span> & <div style="text-align:left; margin:8px 0; color:#0077cc;">
+  04] <span class="explainable" data-group="terms" data-term="">상신</span>,<span class="explainable" data-group="terms" data-term="">구신</span>,<span class="explainable" data-group="terms" data-term="">기신1</span>,<span class="explainable" data-group="terms" data-term="">기신2</span>
+</div>
+<div id="gyeok-flow"></div></td>
        
       </tr>
        <tr>
