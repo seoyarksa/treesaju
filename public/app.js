ef8ccd20ee0390223512ed9bc3c24621a44d0a11
seoyarksa <yyulss@naver.com>
2025-11-05 09:05:42 +0900

    미니창 생서

--- public/app.js
+++ public/app.js
@@ -4924,7 +4924,7 @@ if (secondaryGyeokResult?.primary && secondaryGyeokResult?.secondary) {
   // 생지(복수격)
   gyeokDisplayText = `
     <span id="gyeok-primary" style="cursor:pointer; color:#2277ff;"><b>
-      ${window.gyeokName}${window.gyeokStem ? '' : ''}
+      <span class="explainable" data-group="gyeokook" data-term="">${window.gyeokName}</span>${window.gyeokStem ? '' : ''}
     </b></span>
     <span style="font-size:0.92em;"> (보조격: </span>
     <span id="gyeok-secondary" style="cursor:pointer; color:#ff8844;">
@@ -4987,7 +4987,7 @@ if (gyeokDisplayEl) {
   // 출력은 기존의 주격+보조격 로직(gyeokDisplayText)을 유지
   gyeokDisplayEl.innerHTML = `   <div style="text-align:left; margin:8px 0; color:#0077cc;">
   05] <span class="explainable" data-group="terms" data-term="">격국</span>[&보조격]의 구성 및 도식도
-</div><br><br><span class="explainable" data-group="terms" data-term="">격국</span>: <span class="explainable" data-group="gyeokook" data-term="">${gyeokDisplayText}</span> `;
+</div><br><br><span class="explainable" data-group="terms" data-term="">격국</span>: ${gyeokDisplayText} `;
 
 
 }
